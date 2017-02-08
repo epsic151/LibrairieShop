f17902eb32a231414ba2a7639dfcefb857dd9166
Rael <realrael@hispeed.ch>
2017-02-09 00:40:47 +0100

    TEST FLUX RSS

--- fluxRSS/fluxRssUpdate.php
+++ fluxRSS/fluxRssUpdate.php
@@ -19,29 +19,34 @@
 
 function updateRSSfeeder($title,$author,$description)
 {
+	echo "COUCOU";
 	// Open xml file to insert new entry:
-	if (file_exists('libraryFlux.xml'))
+	if (file_exists($_SERVER['DOCUMENT_ROOT']."/fluxRSS/libraryFlux.xml"))
 	{
+		echo "<p>file exists.</br><p>";
 		// Use DOMdocument parser:
 		$xmlRss = new DOMDocument();
-		$xmlRss->load('libraryFlux.xml');
+		$xmlRss->load($_SERVER['DOCUMENT_ROOT']."/fluxRSS/libraryFlux.xml");
+		echo $xmlRss->saveXML();
 
 		$nodeItem = $xmlRss->createElement("item");
 		$nodeTitle = $xmlRss->createTextNode((string)$title);
-		$nodeAuth = xmlRss>createTextNode((string)$author);
+		$nodeAuth = $xmlRss->createTextNode((string)$author);
 		$nodeDescr = $xmlRss->createTextNode((string)$description);
 		$nodeItem->appendChild($nodeTitle);
 		$nodeItem->appendChild($nodeAuth);
 		$nodeItem->appendChild($nodeDescr);
 		// Get the first element item to insert the new one before:
 		$firstItem = $xmlRss->getElementsByTagName("item")->item(0);
+		echo "<p>Number of elements:".$xmlRss->getElementsByTagName("item")->length."</br><p>";
 		
 		// If more than 10 item, remove last:
 		if ($xmlRss->getElementsByTagName("item")->length == 10 )
 		{
 			$lastItem = $xmlRss->getElementsByTagName("item")->item($xmlRss->getElementsByTagName("item")->length - 1);
 			$xmlRss->removeChild($lastItem);
-			}	
+			}
+		$firstItem->appendChild($nodeItem);
 		$xmlRss->save('libraryFlux.xml');
 		}
 
